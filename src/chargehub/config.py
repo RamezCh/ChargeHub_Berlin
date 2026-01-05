cb8f7457038b3b5e767fff98afde812610cf5c37
Ramez Chreide <ramiz_chreide@outlook.com>
2026-01-05 21:56:09 +0100

    Fix Config

--- src/chargehub/config.py
+++ src/chargehub/config.py
@@ -6,10 +6,7 @@ class ChargeHubConfig:
     # Adjust relative path logic if needed, but __file__ based is safer if config is in src/chargehub
     _PROJECT_ROOT = Path(__file__).parent.parent.parent
     DATA_PATH = _PROJECT_ROOT / "data" / "Ladesaeulenregister.csv"
-    
-    # External Resources
-    GEOJSON_URL = "https://tsb-opendata.s3.eu-central-1.amazonaws.com/plz_berlin.geojson"
-    
+
     # Map Defaults (Berlin)
     MAP_CENTER_LAT = 52.5200
     MAP_CENTER_LNG = 13.4050
